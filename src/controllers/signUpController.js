81b9f0e169a94f9ce74f03d600c5aca2cf1f2a01
gavuolo <gabs.vuolo@gmail.com>
2022-11-23 20:02:25 -0400

    signUp fixed

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,19 +1,26 @@
-import { signUpSchema } from "../middlewares/signUpMiddleware.js";
 import bcrypt from "bcrypt";
 import { connectionUser } from "../database/connection.js";
 
 export async function signUp(req, res) {
     const user = req.body;
-    console.log(user)
-
-    const validation = signUpSchema.validate(user)
-    if(validation.error){
-        return res.sendStatus(422)
-    }
+ 
     try{
+        const emailExist = await connectionUser.findOne({email: user.email})
+        const usernameExist = await connectionUser.findOne({username: user.username})
+        
+        if(emailExist){
+            res.status(409).send({message: "Email já cadastrado!"})
+            return
+        }
+        if(usernameExist){
+            res.status(409).send({message: "Username já existente!"})
+            return
+        }
+
         const hashPassword = bcrypt.hashSync(user.password, 10)
         await connectionUser.insertOne({...user, password: hashPassword})
         res.sendStatus(201)
+
     } catch (error){
         console.log(error)
         res.sendStatus(500)
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { login } from "../controllers/userController.js";
 import { loginMiddleware } from "../middlewares/loginMiddleware.js";
+import { signUp } from"../controllers/signUpController.js"
+import { signUpMiddleware } from "../middlewares/signUpMiddleware.js"
 
 const route = Router();
 
-route.post("/sign-up", signUp)
+route.post("/sign-up", signUpMiddleware, signUp)
 
 route.post("/sign-in", loginMiddleware, login)
 
